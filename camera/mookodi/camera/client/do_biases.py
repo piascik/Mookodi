bfaff52689846894162ac6ba89a96bee1ce2fac5
Chris Mottram <C.Mottram@ljmu.ac.uk>
2021-06-22 11:43:13 +0100

    Changes to logging in do_biases.py  do_darks.py to reduce frequency and use string names for enums.

--- camera/mookodi/camera/client/do_biases.py
+++ camera/mookodi/camera/client/do_biases.py
@@ -10,19 +10,22 @@ def do_biases(exposure_count = 1):
     print ("Doing " + repr(exposure_count) + " biases.")
     c.start_multbias(exposure_count)
     done = False
+    loop_count = 0
     while done == False:
         state = c.get_state()
-        print ("Exposure In Progress:" + repr(state.exposure_in_progress)+ ".")
-        print ("Exposure State:" + repr(state.exposure_state)+ ".")
-        print ("Exposure Count:" + repr(state.exposure_count)+ ".")
-        print ("Exposure Index:" + repr(state.exposure_index)+ ".")
+        if( (loop_count % 10) == 0):
+            print ("Exposure In Progress:" + repr(state.exposure_in_progress)+ ".")
+            print ("Exposure State:" + ExposureState._VALUES_TO_NAMES[state.exposure_state] + ".")
+            print ("Exposure Count:" + repr(state.exposure_count)+ ".")
+            print ("Exposure Index:" + repr(state.exposure_index)+ ".")
         time.sleep(1)
         done = state.exposure_in_progress == False
+        loop_count ++
     filename_list = c.get_image_filenames()
     filename_count = len(filename_list)
     print ("There are " + repr(filename_count) + " Image Filenames:")
-    for s in filename_list:
-        print ("Image Filename:" + s)
+#    for s in filename_list:
+#        print ("Image Filename:" + s)
 
 # parse command line arguments
 #parser = argparse.ArgumentParser()
@@ -40,13 +43,19 @@ readout_speed_list = [ ReadoutSpeed.SLOW, ReadoutSpeed.FAST ]
 gain_list = [ Gain.ONE, Gain.TWO, Gain.FOUR ]
 exposure_count = 10
 for readout_speed in readout_speed_list:
-    print ("Set readout_speed to :" + repr(readout_speed) + ".")
+    print ("Set readout speed to :" + ReadoutSpeed._VALUES_TO_NAMES[readout_speed] + ".")
     c.set_readout_speed(readout_speed)
     for gain in gain_list:
         c.set_gain(gain)
-        print ("Set gain to :" + repr(gain) + ".")
+        print ("Set gain to :" + Gain._VALUES_TO_NAMES[gain] + ".")
         for bin in bin_list:
             print ("Set binning to :(" + repr(bin) + " , " + repr(bin) + ").")
             c.set_binning(bin,bin)
-            print ("Doing " + repr(exposure_count) + " biases with readout speed: " + repr(readout_speed) + ", gain: " + repr(gain) + ", binning: " + repr(bin) + ".")
+            print ("Doing " + repr(exposure_count) + " biases with readout speed: " + ReadoutSpeed._VALUES_TO_NAMES[readout_speed] + ", gain: " + Gain._VALUES_TO_NAMES[gain] + ", binning: " + repr(bin) + ".")
             do_biases(exposure_count)
+            print ("Finished " + repr(exposure_count) + " biases with readout speed: " + ReadoutSpeed._VALUES_TO_NAMES[readout_speed] + ", gain: " + Gain._VALUES_TO_NAMES[gain] + ", binning: " + repr(bin) + ".")
+            filename_list = c.get_image_filenames()
+            filename_count = len(filename_list)
+            print ("There are " + repr(filename_count) + " Image Filenames:")
+            for s in filename_list:
+                print ("Image Filename:" + s)
--- camera/mookodi/camera/client/do_darks.py
+++ camera/mookodi/camera/client/do_darks.py
@@ -10,21 +10,24 @@ def do_darks(exposure_count = 1, exposure_length=1000):
     print ("Doing " + repr(exposure_count) + " darks of exposure length " + repr(exposure_length) + "ms.")
     c.start_multdark(exposure_count,exposure_length)
     done = False
+    loop_count = 0
     while done == False:
         state = c.get_state()
-        print ("Exposure In Progress:" + repr(state.exposure_in_progress)+ ".")
-        print ("Exposure State:" + repr(state.exposure_state)+ ".")
-        print ("Exposure Count:" + repr(state.exposure_count)+ ".")
-        print ("Exposure Index:" + repr(state.exposure_index)+ ".")
-        print ("Elapsed Exposure Length:" + repr(state.elapsed_exposure_length)+ " ms.")
-        print ("Remaining Exposure Length:" + repr(state.remaining_exposure_length)+ " ms.")
+        if( (loop_count % 10) == 0):
+            print ("Exposure In Progress:" + repr(state.exposure_in_progress)+ ".")
+            print ("Exposure State:" + ExposureState._VALUES_TO_NAMES[state.exposure_state] + ".")
+            print ("Exposure Count:" + repr(state.exposure_count)+ ".")
+            print ("Exposure Index:" + repr(state.exposure_index)+ ".")
+            print ("Elapsed Exposure Length:" + repr(state.elapsed_exposure_length)+ " ms.")
+            print ("Remaining Exposure Length:" + repr(state.remaining_exposure_length)+ " ms.")
         time.sleep(1)
         done = state.exposure_in_progress == False
+        loop_count ++
     filename_list = c.get_image_filenames()
     filename_count = len(filename_list)
     print ("There are " + repr(filename_count) + " Image Filenames:")
-    for s in filename_list:
-        print ("Image Filename:" + s)
+#    for s in filename_list:
+#        print ("Image Filename:" + s)
 
 # parse command line arguments
 #parser = argparse.ArgumentParser()
@@ -43,14 +46,20 @@ gain_list = [ Gain.ONE, Gain.TWO, Gain.FOUR ]
 exposure_count = 10
 exposure_length_list = [ 10, 100, 1000, 10000, 60000, 300000, 600000 ]
 for readout_speed in readout_speed_list:
-    print ("Set readout_speed to :" + repr(readout_speed) + ".")
+    print ("Set readout speed to :" + ReadoutSpeed._VALUES_TO_NAMES[readout_speed] + ".")
     c.set_readout_speed(readout_speed)
     for gain in gain_list:
         c.set_gain(gain)
-        print ("Set gain to :" + repr(gain) + ".")
+        print ("Set gain to :" + Gain._VALUES_TO_NAMES[gain] + ".")
         for bin in bin_list:
             print ("Set binning to :(" + repr(bin) + " , " + repr(bin) + ").")
             c.set_binning(bin,bin)
             for exposure_length in exposure_length_list:
-                print ("Doing " + repr(exposure_count) + " darkss of length " + repr(exposure_length) + " ms with readout speed: " + repr(readout_speed) + ", gain: " + repr(gain) + ", binning: " + repr(bin) + ".")
+                print ("Doing " + repr(exposure_count) + " darks of length " + repr(exposure_length) + " ms with readout speed: " + ReadoutSpeed._VALUES_TO_NAMES[readout_speed] + ", gain: " + Gain._VALUES_TO_NAMES[gain] + ", binning: " + repr(bin) + ".")
                 do_darks(exposure_count,exposure_length)
+                print ("Finished " + repr(exposure_count) + " darks of length " + repr(exposure_length) + " ms with readout speed: " + ReadoutSpeed._VALUES_TO_NAMES[readout_speed] + ", gain: " + Gain._VALUES_TO_NAMES[gain] + ", binning: " + repr(bin) + ".")
+                filename_list = c.get_image_filenames()
+                filename_count = len(filename_list)
+                print ("There are " + repr(filename_count) + " Image Filenames:")
+                for s in filename_list:
+                    print ("Image Filename:" + s)
