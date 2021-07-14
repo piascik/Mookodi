5100217086baf6a7232f19e2ffaa6e7223fc11f5
rjs3273 <robert@kandrsmith.org>
2021-07-14 14:32:32 -0600

    Removed exptime from function calls. Needs setting in controller, not in pipeline.

--- pipelines/AcquisitionController.py
+++ pipelines/AcquisitionController.py
@@ -27,12 +27,12 @@ class AcquisitionController(object):
         self.offset_y = None
 
 
-    def acquire_wcs(self, filename, target_ra, target_dec, target_skypa, magic_pix_x, magic_pix_y, xposure=10.0):
+    def acquire_wcs(self, filename, target_ra, target_dec, target_skypa, magic_pix_x, magic_pix_y):
         '''Performs a WCS fit.
         After calling this method, the offsets in seconds of arc on the sky required to place the specified RA,Dec 
         on the specified pixel are available in AcquisitionController.offset_ra and AcquisitionController.offset_dec.
         Coordinates are all specified in a sky reference frame (RA, Dec, arcsec etc).
-        Using integration time (xposure) other than the default generally not recommended since the image needs
+        Using integration times other than the default generally not recommended since the image needs
         to match the photometric depth of the reference catalogues.
         '''
         self.erstat = 0
@@ -40,13 +40,12 @@ class AcquisitionController(object):
         self.offset_dec = -3.1
         return self.erstat
 
-    def acquire_brightest(self, filename, magic_pix_x, magic_pix_y, radius, xposure=1.0):
+    def acquire_brightest(self, filename, magic_pix_x, magic_pix_y, radius):
         '''Runs source detection on image. The target is assumed to be the brightest source detected 
         within the specified radius of the specified magic pixel. 
         After calling this method, the offsets in pixels required to place the assumed target 
         on the specified pixel are available from AcquisitionController.offset_x and AcquisitionController.offset_y. 
         Coordinates are all specified in the instrument focal plane (X,Y, pixels etc).
-        Integration time (xposure) may be changed for bright or faint targets.
         '''
         self.erstat = 0
         self.offset_x = 10.1
